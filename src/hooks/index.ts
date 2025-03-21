70f2574eedcbb52713fe3176ee3b618c9f2d1993
coopdickson91 <97398714+coopdickson91@users.noreply.github.com>
2025-03-21 20:53:04 -0300

    feat(KEY-20): dashboard page (#43)
    Co-authored-by: matthew-collett <matthew.collett12@gmail.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from './use-meter-data'
 export * from './use-now'
-export * from './fetch-events'
+export * from './use-events'
+export * from './use-contracts'
+export * from './use-project-averages'
