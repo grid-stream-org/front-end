4531fc93c51568bcc4b3581a7d3bec388861810e
Eric Cuenat <63607399+Eric599@users.noreply.github.com>
2025-03-22 00:26:02 -0300

    feat(KEY-98): Add notification functionality for contract violation (#44)
    Co-authored-by: Matthew Collett <matthew.collett12@gmail.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,7 @@
 export * from './use-meter-data'
 export * from './use-now'
+export * from './use-notifications'
+export * from './use-mark-notification'
 export * from './use-events'
 export * from './use-contracts'
 export * from './use-project-averages'
